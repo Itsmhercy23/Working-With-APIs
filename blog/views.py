ec83942fe5bedb5df0e1fafeaf8f9c3baadce9ad
itsMhercy <mercyakinwumi36@gmail.com>
2022-08-06 21:23:19 +0100

    newly updated

---
+++ blog/views.py
@@ -0,0 +1,24 @@
+from django.views.generic.edit import CreateView
+from .models import Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostListView(ListView):
+    model = Post
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+class PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
